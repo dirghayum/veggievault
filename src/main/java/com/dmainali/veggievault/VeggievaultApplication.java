a51b8d40f2b57f2cd073750f11f3335268e180e1
dirghayum <dirghayumainali@gmail.com>
2023-03-22 12:36:34 -0600

    Addded java docs

--- src/main/java/com/dmainali/veggievault/VeggievaultApplication.java
+++ src/main/java/com/dmainali/veggievault/VeggievaultApplication.java
@@ -2,7 +2,12 @@ package com.dmainali.veggievault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * The 'VeggievaultApplication' class is the main entry point for the Veggievault application.
+ * This class starts the Spring application context and runs the application.
+ * @author Dirghayu Mainali
+ * @version 1.0
+ */
 @SpringBootApplication
 public class VeggievaultApplication {
 
--- src/main/java/com/dmainali/veggievault/entity/BaseEntity.java
+++ src/main/java/com/dmainali/veggievault/entity/BaseEntity.java
@@ -10,6 +10,12 @@ import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
+/** This class represents a base entity class that provides a common ID field and JPA annotations
+ * for child entities. This class is abstract and cannot be instantiated directly.
+ * @author Dirghayu Mainali
+ * @version 1.0
+ */
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -18,6 +24,9 @@ import java.io.Serializable;
 public abstract class BaseEntity implements Serializable {
     public static final long serialVersionUID = 1L;
 
+    /**
+     * 'id' is the unique identifier for this entity.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Access(AccessType.PROPERTY)
