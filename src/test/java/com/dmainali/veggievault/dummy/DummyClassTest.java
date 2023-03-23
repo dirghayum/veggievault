eb62077a97b5df6e9ad030caf9b48862405fcbed
dirghayum <dirghayumainali@gmail.com>
2023-03-22 20:02:09 -0600

    Added dummy class and test for JenkinsBuildTest

---
+++ src/test/java/com/dmainali/veggievault/dummy/DummyClassTest.java
@@ -0,0 +1,33 @@
+package com.dmainali.veggievault.dummy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class DummyClassTest {
+
+    private DummyClass dummyClass = new DummyClass();
+
+    @BeforeEach
+    public void testName(){
+        dummyClass.result=-999;
+    }
+
+    @Test
+    public void positive(){
+        dummyClass.add(3,4);
+        assertThat(dummyClass.result)
+                .isEqualTo(7);
+    }
+
+    @Test
+    public void negativeTest(){
+        dummyClass.add(6,4);
+        assertThat(dummyClass.result)
+                .isEqualTo(7);
+    }
+
+}
